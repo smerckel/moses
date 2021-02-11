dc768c0f5e4c038cabb3631a033570c05ccfa08e
Lucas Merckelbach <lucas.merckelbach@hzg.de>
2021-02-11 14:46:16 +0100

    Adds some test programs.

--- test_scripts.py
+++ test_scripts.py
@@ -6,12 +6,24 @@ from moses import __main__
 
 import glob
 
-fns = dbdreader.DBDList(glob.glob('/home/lucas/gliderdata/caboverde_201911/ld/dipsy*.sbd'))
-fns.sort()
+if 0: # test 0
+    fns = dbdreader.DBDList(glob.glob('/home/lucas/gliderdata/caboverde_201911/ld/dipsy*.sbd'))
+    fns.sort()
 
 
 
-sys.argv=['main',
-          '--processor_directory=/home/lucas/gliderdata/caboverde_201911/coriolis/matlab',
-          '--id=20191123', '--skip_ftp_transfer'] + fns
-__main__.script_coriolis_upload()
+    sys.argv=['main',
+              '--processor_directory=/home/lucas/gliderdata/caboverde_201911/coriolis/matlab',
+              '--id=20191123', '--skip_ftp_transfer'] + fns
+    __main__.script_coriolis_upload()
+
+
+if 1:
+    fns = dbdreader.DBDList(glob.glob('/home/lucas/gliderdata/caboverde_201911/hd/amadeus*.dbd'))
+    fns.sort()
+
+
+
+    sys.argv=['main',
+              '--processor_directory=/home/lucas/gliderdata/caboverde_201911/coriolis/matlab'] + fns
+    __main__.script_convert_for_coriolis()
