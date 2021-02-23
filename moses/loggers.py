72d82112a484f7c90b75eda35e7a5d3ba087ae02
Lucas Merckelbach <lucas.merckelbach@hzg.de>
2021-02-23 10:41:52 +0100

    Fixes duplicates of log entries.

--- moses/loggers.py
+++ moses/loggers.py
@@ -5,13 +5,10 @@ LEVEL = logging.DEBUG
 def get_logger(name):
     logger = logging.getLogger(name)
     logger.setLevel(LEVEL)
-    if 0:
-        ch = logging.StreamHandler()
-        ch.setLevel(LEVEL)
-        # create formatter
-        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-        # add formatter to ch
-        ch.setFormatter(formatter)
-        # add ch to logger
-        logger.addHandler(ch)
+    ch = logging.StreamHandler()
+    ch.setLevel(LEVEL)
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+    ch.setFormatter(formatter)
+    logger.addHandler(ch)
+    logger.propagate=False
     return logger
